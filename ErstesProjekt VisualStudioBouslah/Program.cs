6997fe01906ecac285a771c5a6aec2bb3a424867
ayoubcgn <77978129+ayoubcgn@users.noreply.github.com>
2021-08-28 12:01:54 +0200

    Projektdateien hinzufügen.

---
+++ ErstesProjekt VisualStudioBouslah/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+//SO schreibt man ein kommentar 
+/*SO schreibt man kommentare die über mehrere zeilen gehen*/
+
+namespace ErstesProjekt_VisualStudioBouslah
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ Tutorial/tutorial1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Tutorial
+{
+    class tutorial1
+    {
+
+        /* Mit 'static' werden Methoden bezeichnet die beim Aufruf kein spezifisches Objekt voraussetzen, 
+        und 'void' beschreibt eine Methode ohne Rückgabewert. 
+        'Main' ist eine Methode von herrausragender Bedeutung. Wenn Laufzeitumgebung der EXE-Datei startet, wird zuerst 'Main' ausgeführt*/
+        static void Main()
+        {
+            Console.WriteLine("Hallo Boss");
+            Console.WriteLine("Wie geht es dir heute ?");
+           String Input = Console.ReadLine();
+            Console.WriteLine("Die Eingabe Ist :");
+            Console.WriteLine(Input);
+
+
+        }
+    }
+}
