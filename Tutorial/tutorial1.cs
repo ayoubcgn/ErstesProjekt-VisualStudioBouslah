26cc4870e45739f4d6a3eebe5cda41fc0f0f550b
ayoubcgn <77978129+ayoubcgn@users.noreply.github.com>
2021-09-01 14:31:30 +0200

    n

--- Tutorial/tutorial1.cs
+++ Tutorial/tutorial1.cs
@@ -10,12 +10,30 @@ namespace Tutorial
         'Main' ist eine Methode von herrausragender Bedeutung. Wenn Laufzeitumgebung der EXE-Datei startet, wird zuerst 'Main' ausgeführt*/
         static void Main()
         {
-            Console.WriteLine("Hallo Boss");
-            Console.WriteLine("Wie geht es dir heute ?");
-           String Input = Console.ReadLine();
-            Console.WriteLine("Die Eingabe Ist :");
-            Console.WriteLine(Input);
+            Console.WriteLine("Hallo...");
 
+            
+            Console.WriteLine("C# macht spaß");
+            
+            Console.ReadLine();
+            
+            Console.ReadKey();
+            
+            int value;
+
+            value = 23;
+
+            Double Value2 = 1000.11;
+            Console.WriteLine(value+Value2);
+            long a = 10,b = 100000000000;
+            Console.WriteLine
+                ("wert von ´a´ = {0} \nWert von ´b´ = {1} +{2}" ,a,b,value );
+
+
+
+
+     
+            
 
         }
     }
