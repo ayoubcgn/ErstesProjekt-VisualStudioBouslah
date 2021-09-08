78e05608d141b075618424f75954984e0ba94ec7
ayoubcgn <77978129+ayoubcgn@users.noreply.github.com>
2021-09-08 15:47:22 +0200

    nahchilfe

---
+++ Nachhilfe/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Nachhilfe
+{
+    class Program
+    {
+        static void Main()
+        {
+            string input1;  
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("HALLO DU HUND");
+            Console.ReadLine();
+            
+
+        }
+    }
+}
